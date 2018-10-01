04efea3a3e265f5e6431a97326bcf87546271f21
plasma0 <konrad_kania@wp.pl>
2018-10-02 00:06:54 +0200

    Added exectuable

---
+++ Main.hs
@@ -0,0 +1,10 @@
+-- author: Konrad Kania
+-- licence: as-is with restriction of commercial use.
+-- this version of presented software has been made only for testing purpose
+
+import Block
+import DBManager
+
+
+
+main = putStrLn "Useage: chainstore file.db --option \n --show Show all records \n --latest Show latest added record\n --add CONTENT Add record with own CONTENT \n --verify Check database corectness"
